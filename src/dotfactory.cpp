9a6c061bf8d1ef2b2ab4a6d40af3f99baac16bcc
kiddo <kiddo.pwn@gmail.com>
2022-11-23 15:35:33 +0900

    feat(DotFactory): add factory to make staticobjects
    - 우선 팩토리를 적용해서 이름과 위치를 받아 item을 찍어주는 팩토리 구현
    - collide detaction과 dynamicobject 적용 관련해서는 추후 변경 필요

--- src/dotfactory.cpp
+++ src/dotfactory.cpp
@@ -1,15 +1,20 @@
 #include "dotfactory.h"
 
 #include "dot.h"
+#include "dotgraphicscomponent.h"
+#include "dotphysicscomponent.h"
 #include "gameobject.h"
 
 DotFactory::DotFactory() {}
 
-DotFactory::DotFactory(PhysicsComponent* physics, GraphicsComponent* graphics)
-    : physics(physics), graphics(graphics) {}
+DotFactory::DotFactory(QGraphicsScene* scene) { this->scene = scene; }
+
+DotFactory::DotFactory(PhysicsComponent* physics, GraphicsComponent* graphics) {
+}
 
 GameObject* DotFactory::CreateObject(QString name, QPoint pos) {
-  Dot* dot = new Dot(name, physics, graphics);
+  Dot* dot = new Dot(name, new DotPhysicsComponent(),
+                     new DotGraphicsComponent(name, *scene));
   dot->SetPos(pos * 20);
 
   return dot;
