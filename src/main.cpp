3e8f86b47c804fc24dfcc6c098d75b5c5330332e
d0422 <rlfehd2013@naver.com>
2022-11-25 16:21:41 +0900

    feat:add custom font

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <QApplication>
+#include <QFontDatabase>
 
+#include "QLabel"
 #include "mainwindow.h"
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
   MainWindow w;
+  int id = QFontDatabase::addApplicationFont(":/res/font/emulogic.ttf");
+  QString family = QFontDatabase::applicationFontFamilies(id).at(0);
+  QFont SerifFont(family);
+  a.setFont(SerifFont);
   w.show();
   w.Intro();
   return a.exec();
